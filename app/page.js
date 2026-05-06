2d64cafb29fab8c930a08e0b0979834a3d49bb7a
aneeshmaury <aneeshmaury.in@gmail.com>
2026-05-06 18:42:17 +0530

    hfghj

--- app/page.js
+++ app/page.js
@@ -247,7 +247,7 @@ export default function Page() {
         </div>
 
         {/* IMAGE */}
-        <div className="flex justify-center mb-3">
+        <div className="flex justify-center ">
 
           <img
             src="https://i.ibb.co/4nZyL5xZ/Chat-GPT-Image-May-6-2026-03-14-49-PM-removebg-preview.png"
