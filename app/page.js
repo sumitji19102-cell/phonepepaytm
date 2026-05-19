6213a7747042889d1e17959a49a6b8fea1b5bc1b
sumitji19102-cell <sumitji19102@gmail.com>
2026-05-19 06:03:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -284,7 +284,7 @@ export default function Page() {
 
         <img
           src="https://i.ibb.co/Kp5QFKVR/1000045820-removebg-preview.png"
-          className="w-[110px] mt-[-6px] "
+          className="w-[110px] mt-[-15px] "
         />
 
       </div>
